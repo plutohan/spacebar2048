82b1f5677670397e0cfdb24f220383edb5769a9d
pluto <pluto@ao.xyz>
2023-10-23 14:16:44 +0900

    encrypt local storage

--- src/hooks/useLocalStorage.ts
+++ src/hooks/useLocalStorage.ts
@@ -1,39 +1,55 @@
-import React, { useEffect, useReducer } from "react";
+import React, { useEffect, useReducer } from "react"
+import CryptoJS from "crypto-js"
 
-const GAME_ID = "2048game";
+const GAME_ID = "space2048!"
+const seed = "hi there"
 
 const useStateReducer = (prevState, newState) => {
-  return typeof newState === "function" ? newState(prevState) : newState;
-};
-
-const getInitialValue = (key: string, defaultValue: any) => {
-  try {
-    const gameState = JSON.parse(window.localStorage.getItem(GAME_ID));
-    const value = gameState?.[key];
-    return value ?? defaultValue;
-  } catch (error) {
-    console.log(error);
-    return defaultValue;
-  }
-};
-
-function useGameLocalStorage<T>(
-  key: string,
-  defaultValue: T,
-  reducer = useStateReducer
-): [T, React.Dispatch<any>] {
-  const [value, dispatch] = useReducer(
-    reducer,
-    getInitialValue(key, defaultValue)
-  );
-
-  useEffect(() => {
-    let state = JSON.parse(window.localStorage.getItem(GAME_ID)) || {};
-    state[key] = value;
-    window.localStorage.setItem(GAME_ID, JSON.stringify(state));
-  }, [value, key]);
-
-  return [value, dispatch];
+	return typeof newState === "function" ? newState(prevState) : newState
 }
 
-export default useGameLocalStorage;
+const getInitialValue = (key: string, defaultValue: any) => {
+	try {
+		const gameState = getGameState()
+		const value = gameState?.[key]
+		return value ?? defaultValue
+	} catch (error) {
+		console.log(error)
+		return defaultValue
+	}
+}
+
+const getGameState = () => {
+	const encrypted = window.localStorage.getItem(GAME_ID)
+	let state = {}
+	if (encrypted) {
+		state = JSON.parse(
+			CryptoJS.AES.decrypt(encrypted, seed).toString(CryptoJS.enc.Utf8)
+		)
+	}
+	return state
+}
+
+function useGameLocalStorage<T>(
+	key: string,
+	defaultValue: T,
+	reducer = useStateReducer
+): [T, React.Dispatch<any>] {
+	const [value, dispatch] = useReducer(
+		reducer,
+		getInitialValue(key, defaultValue)
+	)
+
+	useEffect(() => {
+		let state = getGameState()
+		state[key] = value
+		window.localStorage.setItem(
+			GAME_ID,
+			CryptoJS.AES.encrypt(JSON.stringify(state), seed).toString()
+		)
+	}, [value, key])
+
+	return [value, dispatch]
+}
+
+export default useGameLocalStorage
